96b1171145e3ef5b01cdafc7ae7d62210140c191
gregroberts <greg.roberts1991@gmail.com>
2016-08-11 13:07:31 +0000

    Finished Collector Basics

--- collector.py
+++ collector.py
@@ -1,5 +1,8 @@
 from server_query import ServerQuery
 from datetime import datetime
+from rq import Queue
+from server_config import REDIS_HOST,REDIS_PORT
+from redis import Redis
 
 
 class Collector:
@@ -9,55 +12,76 @@ class Collector:
 	result_set = []
 	log_data = []
 	state = True
-
+	jobs = []
 	def log(self, line, l_type = 'message'):
+		'''writes to the log'''
 		self.log_data.append([str(datetime.now()), line, l_type])
 		print line
 		if l_type == 'error':
 			raise Exception(line)
 
-	def __init__(self, query_name, name = None):
+	def __init__(self, query_name, name = None, queue_name = 'default'):
 		self.collector_name = name or self.collector_name
 		self.log('collector instanciated with name %s' % self.collector_name)
 		self.query_name = query_name
 		self.log('loading query with name %s' % self.query_name)
+		self.redis_conn = Redis(host=REDIS_HOST,port=REDIS_PORT)
+		self.queue = Queue(queue_name, connection=self.redis_conn, async=True)
 		try:
 			self.Query = ServerQuery(uuid = query_name, from_where='library')
 			self.log('loaded query')
 		except Exception as e:
 			self.log('failed to load query, with exception %s' % str(e), 'error')
 		
+		
 	def set_parameters(self, parameter_set):
+		'''provide all the parameters of the collection'''
 		self.parameter_set = parameter_set
 		self.log('loaded %d rows of parameters' % len(self.parameter_set))
 
-	def run_job(self, row):
-		result = self.Query.run(**row)
-		self.log('running job %s' % row)
-		if type(result) == dict:
-			self.result_set.append(result)
-		elif type(result) == list:
-			self.result_set.extend(result)
+	def get_result(self, index):
+		'''checks on the progress of a job. If complete
+			returns the result, else False'''
+		job = self.jobs[index]
+		if job.is_failed:
+			return 'failed'
+		elif job.is_finished:
+			return job.result
 		else:
-			raise Exception('result type %s not recognised' % type(result))
+			return False
+
+	def run_job(self, params):
+		return self.queue.enqueue(
+			collector_job,
+			kwargs={
+				'query_name':self.query_name,
+				'job_params':params
+			})
+
 
 	def run_jobs(self):
 		self.log('running jobs!')
 		for index, job in enumerate(self.parameter_set):
 			self.log('starting job %d' % (index+1))
-			try:
-				self.run_job(job)
-				self.log('job ran successfully')
-			except Exception as e:
-				self.log('job failed with error %s' % str(e), 'error')
+			self.jobs.append(self.run_job(job))
+
+	def check_progress(self):
+		res = map(self.get_result, range(len(self.jobs)))
+		return {
+			'failed':len(filter(lambda x: x=='failed',res)),
+			'not_finished':len(filter(lambda x: x==False, res)),
+			'finished':len(filter(lambda x: x!= False and x!= 'failed', res))
+		}
+	
+	def get_results(self):
+		return map(self.get_result, range(len(self.jobs)))
 
 
 
+def collector_job(query_name, job_params):
+	Query = ServerQuery(uuid = query_name, from_where='library')
+	return Query.run(**job_params)
 
 
-
-
-class CollectorJob:
-	def __init__(self, query_name, job_params):
-		pass
+		
 
