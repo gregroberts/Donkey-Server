95226a7495d09591524b83fee907f224ef128306
Greg Roberts <greg.roberts1991@gmail.com>
2016-08-11 11:06:47 +0100

    added rq

--- server_frontend.py
+++ server_frontend.py
@@ -3,12 +3,30 @@ from flask.ext.classy import FlaskView, route
 import server_config
 from server_query_api import QueryView
 from json import dumps
+import rq_dashboard
 
 
 
 application = Flask(__name__)
  
 QueryView.register(application)
+
+#DonkeyView.register(application)
+@application.errorhandler(500)
+def internal_error(e):
+	print e
+	return Response(
+		response =dumps({'error':str(e)}),
+		status = 500,
+		mimetype = 'application/json'
+	)
+
+application.config['REDIS_HOST'] = server_config.REDIS_HOST
+application.config['REDIS_PORT'] = server_config.REDIS_PORT
+application.config['RQ_POLL_INTERVALL'] = 2000
+application.config['APPLICATION_ROOT'] = server_config.web_prefix
+application.register_blueprint(rq_dashboard.blueprint, url_prefix='/redis_queue')
+
 
 @application.route('/donkey/', defaults={'path':''})
 @application.route('/<path:path>')
@@ -19,13 +37,5 @@ def index_old(path):
 
 if __name__ == '__main__':
 
-	#DonkeyView.register(application)
-	@application.errorhandler(500)
-	def internal_error(e):
-		print e
-		return Response(
-			response =dumps({'error':str(e)}),
-			status = 500,
-			mimetype = 'application/json'
-		)
+
 	application.run('0.0.0.0', debug = True)
