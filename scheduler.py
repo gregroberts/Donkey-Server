d45e6c5f787afeabbab69c421a3958cfcf94022b
Greg Roberts <greg.roberts1991@gmail.com>
2016-08-16 15:21:05 +0100

    began working on scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,101 +1,82 @@
-#This is the scheduler
-#quite simply, you have a sql query which is executed against the database, which returns a set of data.
-#then, you have a query archetype, which has placeholders for all the values you want to use
-
-
-from MySQLdb.cursors import DictCursor
-from copy import copy
 from rq import Queue
-from redis import Redis
-from collector import collection, finish
-import MySQLdb as madb
-import config as donk_conf
-import json, time
+import server_config
+import MySQLdb as mdb
 
-testing = False
-redis_conn = Redis(host = donk_conf.REDIS_HOST,
-			 port = donk_conf.REDIS_PORT)
+def get_sql_conn():
+	conn = mdb.connect(
+		host=server_config.TRGT_SQL_HOST,
+		port=server_config.TRGT_SQL_PORT,
+		user=server_config.TRGT_SQL_USER,
+		passwd=server_config.TRGT_SQL_PSWD,
+		db=server_config.TRGT_SQL_SCHM
+	)
+	return conn
 
+def mk_table(table_name, columns, db_cursor):
+	col_stmnt = ',\n'.join(map(lambda x: '`%s` TEXT(500)' % x,columns))
+	statement = '''CREATE TABLE IF NOT EXISTS `%s`.`%s` (
+		`id` int(11) NOT NULL AUTO_INCREMENT,
+		`Collected` datetime DEFAULT 0,
+		`etl_status` int(2) DEFAULT 0,
+		%s,
+		PRIMARY KEY (`id`)
+	) ENGINE=InnoDB AUTO_INCREMENT=168 DEFAULT CHARSET=latin1;
+	''' % (server_config.TRGT_SQL_SCHM, table_name, col_stmnt)
+	try:
+		db_cursor.execute(statement)
+	except Exception as e:
+		raise(Exception('%s - %s' % (e,statement)))
 
+def insert_res_set(table_name, res):
+	conn = get_sql_conn()
+	c = conn.cursor()
+	if type(res) == list:
+		cols = res[0].keys()
+		col_st = ', '.join(map(lambda x: '%('+x+')s', cols))
+		do = c.executemany
+	elif type(res) == dict:
+		cols = res.keys()
+		col_st = ', '.join(map(lambda x: '%('+x+')s', cols))
+		do = c.execute
+	mk_table(table_name, cols, c)
+	bits = (
+		server_config.TRGT_SQL_SCHM,
+		table_name,
+		','.join(cols), 
+		datetime.now(),
+		col_st
+	)
+	stmnt = 'INSERT INTO %s.%s (Collected, %s) VALUES (\'%s\', %s)' % bits
+	do(stmnt, args = res)
+	conn.commit()
+	c.close()
+	conn.close()
 
-def schedule(redis_conn, _input, archetype, queue_name, collector_name, inputsource = 'sql', limit = 0, db_conn = None):
-	'''This guy does what it says on the tin. 
-	creates a list of jobs for rq, and adds them to the specified queue
-	returns set of jobs, if you want to check on them
-	'''
-	#set async = false for testing
-	if db_conn is None:
-		db_conn= madb.connect(host=donk_conf.MySQL_host,
-				    user=donk_conf.MySQL_user,
-				    passwd=donk_conf.MySQL_passwd,
-				    port=donk_conf.MySQL_port,
-				    db=donk_conf.MySQL_db)
-	q = Queue(queue_name, connection = redis_conn, async= not testing)
-	archetype = archetype.replace('"','\\\"')
-	db_cursor = db_conn.cursor(cursorclass = DictCursor)
-	if inputsource == 'sql':
-		db_cursor.execute(_input)
-		results = db_cursor.fetchall()
-	elif inputsource == 'json':
-		if isinstance(_input, basestring):
-			results = json.loads(_input)
-		else:
-			results = _input
-	job_rets = []
-	if limit != 0:
-		results = results[:limit]
-	db_cursor.execute('''INSERT INTO Collections_Log
-			(CollectorName, JobName, TimeStarted,Jobs)
-			VALUES ('%s','%s',NOW(),%d)
-		''' % (collector_name.split('-')[0], collector_name, len(results)))
-	db_conn.commit()
-	for index, row in enumerate(results):
-		job_name = '%s-%d' % (collector_name, index)
-		job = copy(archetype)
-		for col_n, col_v in row.items():
-			job = job.replace('{{%s}}' % col_n, str(col_v))
-		job = json.loads(job.decode('string-escape'))
-		res = q.enqueue(collection, job,collector_name, job_id= job_name)
-		job_rets.append(res)
-	print finish, collector_name, len(results)
-	#finally, add a job which finishes the collection
-	q.enqueue(finish, collector_name, len(results))
-	return job_rets
-
-def scheduler(which = None, db_conn=None):
-	'''schedules all the collections which need doing'''
-	if db_conn is None:
-		db_conn= madb.connect(host=donk_conf.MySQL_host,
-				    user=donk_conf.MySQL_user,
-				    passwd=donk_conf.MySQL_passwd,
-				    port=donk_conf.MySQL_port,
-				    db=donk_conf.MySQL_db)
-	cursor = db_conn.cursor(cursorclass = DictCursor)
-	if which is None:
-		cursor.execute('''SELECT * FROM Collections
-					 WHERE DATE_ADD(LastScheduled,INTERVAL Frequency DAY) <= curdate()
-					 and InProgress = 0
-					 ''')
-	else:
-		cursor.execute('''SELECT * from Collections
-			where CollectorName in (%s)
-			''' % ','.join(map(lambda x: '"%s"' % x, which)))
-	jobs = cursor.fetchall()	
-	for i in jobs:
-		collector_name = '%s-%d' % (i['CollectorName'] , time.time())
-		schedule(redis_conn,
-			   i['Input'],
-			   i['Archetype'],
-			   i['QueueName'],
-			   collector_name,
-			   i['InputSource']
-			)
-		cursor.execute('''UPDATE Collections 
-					SET LastScheduled = curdate() ,
-						InProgress = 1
-					WHERE CollectorName = \'%s\' ''' % i['CollectorName'])
-		db_conn.commit()
+def consume_data(job_id, table_name):
+	results = job_id.result
+	#if failed, fail die gracefully, log fail
+	insert_res_set(table_name, results)
 
 
-if __name__ == '__main__':
-	scheduler()
+class Collection:
+	def __init__(self, collection_name, query_name, queue_name, table_name):
+		self.collection_name = collection_name
+		self.queue = rq.Queue('collections')
+		self.query_name = query_name
+		self.collector = Collector(query_name, collection_name, queue_name)
+
+	def schedule(self, job_parameters):
+		self.collector.set_parameters(job_parameters)
+		self.collector.run_jobs()
+		map(self.add_finisher, self.collector.jobs)
+
+	def add_finisher(self, job):
+		self.queue.enqueue(
+			consume_data,
+			kwargs = {'job_id':uuid,'table_name':self.table_name},
+			depends_on = job
+		)
+
+
+
+
